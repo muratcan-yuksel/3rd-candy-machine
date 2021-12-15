2b0a2178f22bc9d4dc3b54c450af06f1574455c9
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-15 14:27:54 +0300

    gh pages

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import aboutImg from "../assets/about.png";
 
 const About = () => {
   return (
     <div id="aboutComponent">
       <Container>
         <Row>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
+          <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6}>
+            <div id="aboutImgContainer">
+              <img src={aboutImg} alt="" id="aboutImg" />
+            </div>
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6}>
+            2 of 2
+          </Col>
         </Row>
       </Container>
     </div>
