537af306e933ce1b402ff6e9fbe4d2817179c8e5
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-15 15:15:20 +0300

    .

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/roadmap.css";
 
 const Roadmap = () => {
-  return <div id="roadmapComponent"></div>;
+  return <div id="roadmapComponent">s</div>;
 };
 
 export default Roadmap;
