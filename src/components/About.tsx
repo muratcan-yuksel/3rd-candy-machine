25c376f7f1ceec4627f099e5ee15790bec890b42
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-15 14:35:31 +0300

    add about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import aboutImg from "../assets/about.png";
+import "../styles/about.css";
 
 const About = () => {
   return (
@@ -13,7 +14,22 @@ const About = () => {
             </div>
           </Col>
           <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6}>
-            2 of 2
+            <div id="aboutTextGroup">
+              <h2 id="aboutTitle">About</h2>
+              <h3 id="aboutWelcome">Welcome to our place </h3>
+              <p className="aboutPara">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                quod, dolores, odio libero voluptate aliquid quibusdam
+                perferendis eius doloremque impedit rerum molestiae provident
+                voluptatibus, ipsa sunt illo sed atque autem!
+              </p>
+              <p className="aboutPara">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                quod, dolores, odio libero voluptate aliquid quibusdam
+                perferendis eius doloremque impedit rerum molestiae provident
+                voluptatibus, ipsa sunt illo sed atque autem!
+              </p>
+            </div>
           </Col>
         </Row>
       </Container>
