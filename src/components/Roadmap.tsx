6ea64b3e0f5bb477095475d0871c35d533248b44
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-15 15:26:38 +0300

    add roadmap

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -1,8 +1,109 @@
 import React from "react";
 import "../styles/roadmap.css";
+import { Container, Row, Col } from "react-bootstrap";
+import roadmapImg from "../assets/3.png";
 
 const Roadmap = () => {
-  return <div id="roadmapComponent">s</div>;
+  return (
+    <div id="roadmapComponent">
+      <Container>
+        <Row>
+          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+            <div className="roadmapContainer">
+              <div className="roadmapImgContainer">
+                <img src={roadmapImg} alt="" className="roadmapImg" />
+              </div>
+              <div className="roadmapBox">
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  qui voluptatem atque fugit blanditiis. Eveniet aperiam
+                  assumenda adipisci impedit pariatur mollitia! Magni dolor
+                  maiores aut numquam nihil quos aperiam beatae.
+                </p>
+              </div>
+            </div>
+          </Col>{" "}
+          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+            <div className="roadmapContainer">
+              <div className="roadmapImgContainer">
+                <img src={roadmapImg} alt="" className="roadmapImg" />
+              </div>
+              <div className="roadmapBox">
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  qui voluptatem atque fugit blanditiis. Eveniet aperiam
+                  assumenda adipisci impedit pariatur mollitia! Magni dolor
+                  maiores aut numquam nihil quos aperiam beatae.
+                </p>
+              </div>
+            </div>
+          </Col>{" "}
+          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+            <div className="roadmapContainer">
+              <div className="roadmapImgContainer">
+                <img src={roadmapImg} alt="" className="roadmapImg" />
+              </div>
+              <div className="roadmapBox">
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  qui voluptatem atque fugit blanditiis. Eveniet aperiam
+                  assumenda adipisci impedit pariatur mollitia! Magni dolor
+                  maiores aut numquam nihil quos aperiam beatae.
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+            <div className="roadmapContainer">
+              <div className="roadmapImgContainer">
+                <img src={roadmapImg} alt="" className="roadmapImg" />
+              </div>
+              <div className="roadmapBox">
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  qui voluptatem atque fugit blanditiis. Eveniet aperiam
+                  assumenda adipisci impedit pariatur mollitia! Magni dolor
+                  maiores aut numquam nihil quos aperiam beatae.
+                </p>
+              </div>
+            </div>
+          </Col>{" "}
+          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+            <div className="roadmapContainer">
+              <div className="roadmapImgContainer">
+                <img src={roadmapImg} alt="" className="roadmapImg" />
+              </div>
+              <div className="roadmapBox">
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  qui voluptatem atque fugit blanditiis. Eveniet aperiam
+                  assumenda adipisci impedit pariatur mollitia! Magni dolor
+                  maiores aut numquam nihil quos aperiam beatae.
+                </p>
+              </div>
+            </div>
+          </Col>{" "}
+          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+            <div className="roadmapContainer">
+              <div className="roadmapImgContainer">
+                <img src={roadmapImg} alt="" className="roadmapImg" />
+              </div>
+              <div className="roadmapBox">
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+                  qui voluptatem atque fugit blanditiis. Eveniet aperiam
+                  assumenda adipisci impedit pariatur mollitia! Magni dolor
+                  maiores aut numquam nihil quos aperiam beatae.
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Roadmap;
