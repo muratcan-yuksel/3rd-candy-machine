34ab0ff69a48501dbdb6a4a50f28a6240b9ff8df
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-15 15:42:26 +0300

    delete examples container

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -6,30 +6,28 @@ import "../styles/examples.css";
 const Examples = () => {
   return (
     <div id="examplesComponent">
-      <Container>
-        <Row>
-          <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
-            <div className="exampleImgContainer">
-              <img src={exampleImg} alt="" className="exampleImg" />
-            </div>
-          </Col>{" "}
-          <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
-            <div className="exampleImgContainer">
-              <img src={exampleImg} alt="" className="exampleImg" />
-            </div>
-          </Col>{" "}
-          <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
-            <div className="exampleImgContainer">
-              <img src={exampleImg} alt="" className="exampleImg" />
-            </div>
-          </Col>{" "}
-          <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
-            <div className="exampleImgContainer">
-              <img src={exampleImg} alt="" className="exampleImg" />
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
+          <div className="exampleImgContainer">
+            <img src={exampleImg} alt="" className="exampleImg" />
+          </div>
+        </Col>{" "}
+        <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
+          <div className="exampleImgContainer">
+            <img src={exampleImg} alt="" className="exampleImg" />
+          </div>
+        </Col>{" "}
+        <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
+          <div className="exampleImgContainer">
+            <img src={exampleImg} alt="" className="exampleImg" />
+          </div>
+        </Col>{" "}
+        <Col xs={6} sm={6} md={3} lg={3} xl={3} xxl={3}>
+          <div className="exampleImgContainer">
+            <img src={exampleImg} alt="" className="exampleImg" />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
